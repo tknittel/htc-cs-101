1c926118e2c751b874dee6f79bf1f896d2bc0c3e
Forknife <47368998+tknittel@users.noreply.github.com>
2019-02-19 19:27:12 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Week_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pick a number between 1 and 10");
+            int guessedNumber = Convert.ToInt32(Console.ReadLine());
+
+            if (guessedNumber > 4)
+            {
+                Console.WriteLine("Too high!");
+            }
+            else if (guessedNumber < 4)
+            {
+                Console.WriteLine("Too low!");
+            }
+            else
+            {
+                Console.WriteLine("Just right!");
+            }
+        }
+    }
+}
